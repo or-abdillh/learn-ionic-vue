653975e9ad6649dc509446fdc60a942726662a79
or abdillh <hans.abdillh05@gmail.com>
2022-01-28 16:27:28 +0800

    create card component using ionic ui component

---
+++ src/components/Card.vue
@@ -0,0 +1,78 @@
+<script>
+import { defineComponent } from 'vue'
+import { IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/vue'
+
+export default defineComponent({
+	name: 'CustomCard',
+	components: {
+	  IonCard
+	},
+	props: {
+		fullname: {
+			type: String,
+			default: 'Your fullname'
+		},
+		account: {
+			type: String,
+			default: 'Your account'
+		},
+		quote: {
+			type: String,
+			default: 'Your quote'
+		},
+		avatar: {
+			type: String,
+			default: '/assets/profile.jpg'
+		}	
+	},
+	setup() {
+
+		return {  }
+	}
+})
+</script>
+
+<template>
+  <IonCard>
+	<section class="card">
+		<div class="card-header">
+			<strong>Profile</strong>
+			<small>Your personal information will be displayed here</small>
+		</div>
+		<div class="card-body">
+			<img :src="avatar" width="100" />
+			<p>{{ fullname }}</p>
+			<small>{{ account }}</small>
+			<small>{{ quote }}</small>
+		</div>
+	</section>    
+  </IonCard>
+</template>
+
+<style scoped>
+
+.card p {
+	font-size: 1.35rem;
+}
+
+.card strong, .card small {
+	font-size: 1.15rem;
+}
+
+.card-header, .card-body {
+	padding: .75rem 1rem;;
+}
+
+.card-header, .card-body {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+}
+
+.card-body img {
+	border-radius: 50%;
+	width: 45%;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Learn Ionic</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+        <template v-for="( person, i ) in persons" :key="i">
+			<Card :fullname="person.fullname" :account="person.account" :quote="person.quote" />
+        </template>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import Card from '@/components/Card.vue'
+
+export default defineComponent({
+  name: "HomePage",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    Card
+  },
+  setup() {
+  	const persons = [
+  		{
+  			fullname: 'sandhika Galih',
+  			account: '@sandhika_galihh',
+  			quote: 'Jangan lupa titik koma'
+  		},
+  		{
+  			fullname: 'Oka Rajeb Abdillah',
+  			account: '@or_abdillh',
+  			quote: 'Keep calm and santuy'
+  		},
+  		{
+  			fullname: 'Fitriana',
+  			account: '@ampitz09',
+  			quote: 'Be your self and never surrender'
+  		}
+  	];
+
+  	return { persons }
+  }
+});
+</script>
+
+<style scoped>
+
+
+</style>
